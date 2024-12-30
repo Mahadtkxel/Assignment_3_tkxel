d58e215ccbb3717d900bd55b6fc8cdd32ec18de7
mahadtkxel <mahad.karim@tkxel.io>
2024-12-30 12:23:19 +0500

    updated to single var cred

--- terraform_files/main.tf
+++ terraform_files/main.tf
@@ -11,10 +11,10 @@ terraform {
 provider "azurerm" {
    
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  subscription_id = jsondecode(var.azure_credentials).subscriptionId
+  client_id       = jsondecode(var.azure_credentials).clientId
+  client_secret   = jsondecode(var.azure_credentials).clientSecret
+  tenant_id       = jsondecode(var.azure_credentials).tenantId
   # skip_provider_registration = true
 }
 
--- terraform_files/variables.tf
+++ terraform_files/variables.tf
@@ -1,21 +1,6 @@
-variable "subscription_id" {
+variable "azure_credentials" {
+  description = "Azure credentials in JSON format"
   type        = string
-  description = "The Azure subscription ID"
-}
-
-variable "client_id" {
-  type        = string
-  description = "The Azure client ID"
-}
-
-variable "client_secret" {
-  type        = string
-  description = "The Azure client secret"
-}
-
-variable "tenant_id" {
-  type        = string
-  description = "The Azure tenant ID"
 }
 
 

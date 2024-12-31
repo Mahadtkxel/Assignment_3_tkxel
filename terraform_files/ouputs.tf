8f0239721a029cd23800ba138d85be49bb87c810
mahadtkxel <mahad.karim@tkxel.io>
2024-12-31 15:53:32 +0500

    rm container output

--- terraform_files/ouputs.tf
+++ terraform_files/ouputs.tf
@@ -3,10 +3,3 @@ output "public_ip_address" {
   description = "Public ip of VM"
 }
 
-output "storage_account_name" {
-value = azurerm_storage_account.stor_acc_tkxelassign3.name
-}
-
-output "storage_container_name" {
-value = azurerm_storage_container.tfstate.name
-}

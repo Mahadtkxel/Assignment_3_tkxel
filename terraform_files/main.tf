4aa41bc96c7b61e76dbe6c4c2f7736c42b28c99e
mahadtkxel <mahad.karim@tkxel.io>
2024-12-31 16:29:03 +0500

    updated depeonds_on

--- terraform_files/main.tf
+++ terraform_files/main.tf
@@ -71,7 +71,7 @@ resource "azurerm_network_interface" "nic-tkxelassign3" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.terraform_public_ip.id
   }
-  depends_on = [ azurerm_network_interface_security_group_association.nsg_association_terraform, azurerm_network_security_group.terraform_nsg ]
+  depends_on = [ azurerm_network_interface_security_group_association.nsg_association_terraform ]
 }
 
 # Create Network Security Group and rule
